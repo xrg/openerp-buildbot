c7a814b6e2bf3c3b467449757f50cbbc339bc0c9
P. Christeas <p_christ@hol.gr>
2010-08-14 10:38:58 +0300

    status-web: Fix calling of setupUsualPages()

--- openerp_buildbot_master/openobject/status/web.py
+++ openerp_buildbot_master/openobject/status/web.py
@@ -488,8 +488,8 @@ class OpenObjectWebStatus(WebStatus):
     def __init__(self, http_port=None, distrib_port=None, allowForce=False):
         WebStatus.__init__(self, http_port=http_port, distrib_port=distrib_port, allowForce=allowForce)
 
-    def setupUsualPages(self, *args, **kargs):
-        WebStatus.setupUsualPages(self)
+    def setupUsualPages(self, *args, **kwargs):
+        WebStatus.setupUsualPages(self, *args, **kwargs)
         self.putChild("buggraph", BugGraph())
         self.putChild("latestbuilds", LatestBuilds())
         self.putChild("buildersresource", OpenObjectBuildersResource())
