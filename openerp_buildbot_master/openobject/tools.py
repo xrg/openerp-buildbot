7529500dee1a29c4ae79fdd256c5b4a0f99681a2
P. Christeas <p_christ@hol.gr>
2010-09-19 23:26:10 +0300

    tools: a thread-safe pool of resources
    This is a helper, generic class that provides a pool of resources, from
    which we can borrow one and exclusively use it for a while.

--- openerp_buildbot_master/openobject/tools.py
+++ openerp_buildbot_master/openobject/tools.py
@@ -46,4 +46,113 @@ def _to_decode(s):
 
 ustr = _to_unicode
 
+from threading import Condition
+
+class Pool(object):
+    """ A pool of resources, which can be requested one at-a-time
+    """
+    
+    def __init__(self, iter_constr, check_fn=None):
+        """ Init the pool
+            @param iter_constr is an iterable, that can construct a new
+                resource in the pool. It will be called lazily, when more
+                resources are needed
+            @param check_fn  A callable to use before borrow or after free,
+                which will let discard "bad" resources. If check_fn(res)
+                returns False, res will be removed from our lists.
+        """
+        self.__free_ones = []
+        self.__used_ones = []
+        self.__lock = Condition()
+        self.__iterc = iter_constr
+        assert self.__iterc
+        self.__check_fn = check_fn
+        
+    def borrow(self, blocking=False):
+        """Return the next free member of the pool
+        """
+        self.__lock.acquire()
+        while(True):
+            ret = None
+            if len(self.__free_ones):
+                ret = self.__free_ones.pop()
+                if self.__check_fn is not None:
+                    self.__lock.release()
+                    if not self.__check_fn(ret):
+                        ret = None
+                    # An exception will also propagate from here,
+                    # with the lock released
+                    self.__lock.acquire()
+                if ret is None:
+                    continue # the while loop. Ret is at no list any more
+                self.__used_ones.append(ret)
+                self.__lock.release()
+                return ret
+            
+            # no free one, try to construct a new one
+            try:
+                self.__lock.release()
+                ret = self.__iterc.next()
+                # the iterator may temporarily return None, which
+                # means we should wait and retry the operation.
+                self.__lock.acquire()
+                if ret is not None:
+                    self.__used_ones.append(ret)
+                    self.__lock.release()
+                    return ret
+            except StopIteration:
+                if not blocking:
+                    raise ValueError("No free resource")
+                # else pass
+
+            if isinstance(blocking, (int, float)):
+                twait = blocking
+            else:
+                twait = None
+            if (not twait) and not len(self.__free_ones):
+                twait = 10.0 # must continue cycle at some point!
+            self.__lock.wait(twait) # As condition
+            if not len(self.__free_ones):
+                raise ValueError("Timed out waiting for a free resource")
+            continue
+
+        raise RuntimeError("Should never reach here")
+        
+    def free(self, res):
+        self.__lock.acquire()
+        try:
+            self.__used_ones.remove(res)
+        except ValueError:
+            self.__lock.release()
+            raise RuntimeError("Strange, freed pool item that was not in the list")
+        if self.__check_fn is not None:
+            self.__lock.release()
+            if not self.__check_fn(res):
+                res = None
+                # not append to free ones, but issue notification
+            # An exception will also propagate from here,
+            # with the lock released
+            self.__lock.acquire()
+        if res is not None:
+            self.__free_ones.append(res)
+        self.__lock.notify_all()
+        self.__lock.release()
+        
+    def __len__(self):
+        return len(self.__free_ones) + len(self.__used_ones)
+
+    def count_free(self):
+        return len(self.__free_ones)
+
+    def clear(self):
+        """ Forgets about all resources.
+        Warning: if you ever use this function, you must make sure that
+        the iterable will catch up and restart iteration with more resources
+        """
+        self.__lock.acquire()
+        self.__free_ones = []
+        self.__used_ones = []
+        self.__lock.notify_all() # Let them retry
+        self.__lock.release()
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
