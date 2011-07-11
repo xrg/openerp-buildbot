2552e570aa21a8d4a8df6653aea8dac00f954585
P. Christeas <xrg@linux.gr>
2011-07-11 20:14:51 +0300

    steps: RPM build step, for git-build
    git-build means the code repository is self-sufficient for a full RPM
    build, including the correct versioning etc. So, the step will merely
    have to issue the 'rpmbuild' command with the right path to the spec
    file.
    
    i can haz RPMs !

---
+++ openerp_buildbot_master/bbot_oe/buildsteps/rpmbuild.py
@@ -0,0 +1,120 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Portions Copyright Buildbot Team Members
+# Portions Copyright Dan Radez <dradez+buildbot@redhat.com>
+# Portions Copyright Steve 'Ashcrow' Milner <smilner+buildbot@redhat.com>
+# Portions Copyright P. Christeas <xrg@linux.gr>
+
+"""
+    RPM Building steps.
+
+    Git-Build variant: this is a fork of the upstream buildbot class,
+    adapted to automated RPM building.
+"""
+
+from buildbot.steps.shell import ShellCommand
+from buildbot.process.buildstep import RemoteShellCommand
+from bbot_oe.step_iface import StepOE
+
+
+class RpmBuild2(StepOE, ShellCommand):
+    """ Build an RPM from a .spec file inside the repository
+    
+        This step is adapted to "git-build" procedure, as supported
+        by the git-rpmscripts in:
+        http://git.hellug.gr/?p=xrg/gitscripts
+        
+        However, there is no explicit dependency on them. Any package
+        with an embedded .spec file can be built (as long as the environment
+        at the buildslave is also ready) like that.
+    """
+
+    name = "rpmbuilder"
+    haltOnFailure = 1
+    flunkOnFailure = 1
+    description = ["RPMBUILD"]
+    descriptionDone = ["RPMBUILD"]
+
+    def __init__(self, workdir=None, buildmode='ba', specfile=None, keeper_conf=None, **kwargs):
+        """
+        @type specfile: str
+        @param specfile: the name of the spec file for the rpmbuild
+        @type kwargs: dict
+        @param kwargs: All further keyword arguments.
+        
+        Note: since the spec file will most usually be per project, aka. per 'component',
+        this class can read the spec file location of the /component parts/, from a
+        part that will (strictly) substitute 'spec' and have the search regex be
+        the path to the spec file *unmodified*.
+        """
+        # TODO extra arguments
+        ShellCommand.__init__(self, workdir=workdir, **kwargs)
+        StepOE.__init__(self, workdir=workdir, keeper_conf=keeper_conf, **kwargs)
+        if keeper_conf and not specfile:
+            print "Trying to get spec file from components"
+            for comp, rege_str, subst in keeper_conf['builder'].get('component_parts',[]):
+                if subst == 'spec':
+                    specfile = rege_str
+                    print "Got SpecFile:", specfile
+                    break
+        self.remote_kwargs['workdir'] = self.workdir
+        self.addFactoryArguments(specfile=specfile, workdir=self.workdir, buildmode=buildmode)
+        self.specfile = specfile
+        self.buildmode = buildmode
+
+    def start(self):
+        """
+        Buildbot Calls Me when it's time to start
+        """
+
+        if self.remote_kwargs.get('workdir') is None:
+            self.remote_kwargs['workdir'] = self.workdir
+
+        self.command = ['rpmbuild', '-' + self.buildmode, self.specfile]
+
+        # create the actual RemoteShellCommand instance now
+
+        kwargs = self.remote_kwargs
+        kwargs['command'] = self.command
+        cmd = RemoteShellCommand(**kwargs)
+        self.setupEnvironment(cmd)
+        self.checkForOldSlaveAndLogfiles()
+        self.startCommand(cmd)
+
+    def createSummary(self, log):
+        """
+        Create nice summary logs.
+
+        @param log: The log to create summary off of.
+        """
+        rpm_prefixes = ['Provides:', 'Requires(rpmlib):', 'Requires:',
+                        'Checking for unpackaged', 'Wrote:',
+                        'Executing(%', '+ ']
+        rpm_err_pfx = ['   ', 'RPM build errors:', 'error: ']
+
+        rpmcmdlog = []
+        rpmerrors = []
+
+        for line in log.readlines():
+            for pfx in rpm_prefixes:
+                if pfx in line:
+                    rpmcmdlog.append(line)
+            for err in rpm_err_pfx:
+                if err in line:
+                    rpmerrors.append(line)
+        self.addCompleteLog('RPM Command Log', "".join(rpmcmdlog))
+        self.addCompleteLog('RPM Errors', "".join(rpmerrors))
+
+exported_buildsteps = [RpmBuild2, ]
+#eof
