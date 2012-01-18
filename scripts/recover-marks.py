cbec65da6d679dd40bba63ab5d46068687e96d59
P. Christeas <xrg@linux.gr>
2012-01-18 11:10:23 +0200

    recover-marks: rearrange missing marks by common parents
    This is the next part of the algorithm, that tries to revert
    damage done by re-exporting mirrored commits. It tries to
    locate the original mark, as indicated by children's parents
    and reset that.

--- scripts/recover-marks.py
+++ scripts/recover-marks.py
@@ -106,7 +106,7 @@ def get_commits(ids, fields=None):
     if fields is None:
         fields = ['date', 'description', 'branch_id', 'hash',
                 'commitmap_id', 'contained_commit_ids', 'ctype',
-                'parent_id', 'subject']
+                'parent_id', 'subject', 'date']
 
     log.debug("Fetch commits: %r", ids)
     ret = {}
@@ -212,11 +212,15 @@ def main_loop():
   
     do_write = options.opts.do_write
     did_something = False
-    for bad_mark in marks_obj.search_read([('collection_id', '=', coll_id), ('verified', 'not in', ('ok', 'unknown'))],
+    skip_marks = []
+    for bad_mark in marks_obj.search_read([('collection_id', '=', coll_id), ('verified', 'not in', ('ok', 'unknown', 'bad'))],
                 fields=['mark', 'commit_ids', 'verified'],
                 order='id', limit=options.opts.limit):
         log.debug("Found a %s mark: %s", bad_mark['verified'], bad_mark['mark'])
     
+        if bad_mark['id'] in skip_marks:
+            continue
+
         if bad_mark['verified'] == 'bad-missing':
             commits = get_commits(bad_mark['commit_ids'])
             if not len(commits):
@@ -229,25 +233,102 @@ def main_loop():
                         if len(alts) > 1 or bad_mark['mark'] not in alts:
                             log.info("Located alternatives for %s: %r", bad_mark['mark'], alts)
                             handled = True
+                        c['alt_marks'] = alts
                     elif c['rtype'] == 'bzr':
                         alts = bzr_marks2.get(c['hash'], [])
                         if len(alts) > 1 or bad_mark['mark'] not in alts:
                             log.info("Located alternatives for %s: %r", bad_mark['mark'], alts)
                             handled = True
+                        c['alt_marks'] = alts
                     else:
                         log.warning("No rtype for commit %d !", c['id'])
                 
                 if not handled:
                     # second check: walk one child down and see if it is a leaf node
-                    children = commit_obj.search_read([('parent_id','in', bad_mark['commit_ids']),
-                            ('commitmap_id','!=', False)], fields=['commitmap_id', 'hash', 'branch_id'])
+                    children = commit_obj.search_read(['|',('parent_id','in', bad_mark['commit_ids']),
+                            ('contained_commit_ids', 'in', bad_mark['commit_ids']),
+                            ('commitmap_id','!=', False)], 
+                            fields=['commitmap_id', 'hash', 'branch_id', 'parent_id', 'contained_commit_ids'])
                     if not children:
                         log.info("Mark %s is a leaf, can be pruned!", bad_mark['mark'])
                         handled = True
                         if do_write:
                             marks_obj.unlink(bad_mark['id'])
                             did_something = True
+                    else:
+                        # Try to discover the corresponding
+                        # go down one child, and then find if that child's parents
+                        # point to a different mark..
+                        # print "num children:", len(children)
+                        replace_case = {}
+                        for ch in children:
+                            cmt = False
+                            # locate which of our commits this does reference
+                            if ch['parent_id'] and ch['parent_id'][0] in commits:
+                                cmt = commits[ch['parent_id'][0]]
+                            else:
+                                for par in ch['contained_commit_ids']:
+                                    if par in commits:
+                                        cmt = commits[par]
+                                        break
+                            if not cmt:
+                                log.warning("Cannot locate parent, strange! %s %r",
+                                        ch['parent_id'], ch['contained_commit_ids'])
+                                break
+                            
+                            # now, find the marks of those children, and jump to their
+                            # parents back
+                            alt_children = commit_obj.search_read([('commitmap_id', '=', ch['commitmap_id'][0]),
+                                    ('branch_id', '!=', ch['branch_id'][0])], 
+                                    fields=['parent_id', 'contained_commit_ids'])
+                            # note: they would be mixed among 2nd, 3rd... repo
+                            alt_parents = []
+                            for ac in alt_children:
+                                log.debug("alt_children: %r", alt_children)
+                                if ac['parent_id']:
+                                    alt_parents.append(ac['parent_id'][0])
+                                if ac['contained_commit_ids']:
+                                    alt_parents.extend(ac['contained_commit_ids'])
+                            if alt_parents:
+                                log.debug("alt parents: %r", alt_parents)
+                                alt_commits = commit_obj.search_read([('id', 'in', alt_parents),
+                                        ('date', '=', cmt['date']),('subject', '=', cmt['subject'])],
+                                        fields=['branch_id', 'commitmap_id', 'hash'])
+                                
+                                if alt_commits:
+                                    log.debug("Located alt commits for mark: %d %s %s", 
+                                            bad_mark['id'], bad_mark['mark'], known_branches[cmt['branch_id'][0]]['rtype'])
+                                    log.debug("Instead of %d %s: %s %s", cmt['id'], cmt['hash'], 
+                                            cmt['commitmap_id'][1], cmt.get('alt_marks','-'))
+                                    for atc in alt_commits:
+                                        log.debug(" also have %d %s: %s", atc['id'], atc['hash'], atc['commitmap_id'])
+                                
+                                        if replace_case and ((not atc['commitmap_id']) \
+                                                or replace_case['alt_mark'] != atc['commitmap_id'][0]):
+                                            replace_case['borked'] = True
+                                        elif atc['commitmap_id'] and not replace_case:
+                                            replace_case = dict(orig_mark=bad_mark['id'],
+                                                    orig_mark_s=bad_mark['mark'],
+                                                    alt_mark=atc['commitmap_id'][0],
+                                                    alt_mark_s=atc['commitmap_id'][1],
+                                                    move_commits=[atc['id'],])
+                                        elif replace_case:
+                                            replace_case['move_commits'].append(atc['id'])
                 
+                        if replace_case:
+                            if replace_case.get('borked', False):
+                                log.warning("Cannot merge %s into %s", replace_case['alt_mark_s'], replace_case['orig_mark_s'])
+                            else:
+                                log.info("Will merge %s into %s (commits: %s)", 
+                                        replace_case['alt_mark_s'], replace_case['orig_mark_s'], replace_case['move_commits'])
+                                handled = True
+                                skip_marks.append(replace_case['alt_mark'])
+                                
+                                if do_write:
+                                    commit_obj.write(replace_case['move_commits'], {'commitmap_id': replace_case['orig_mark']})
+                                    marks_obj.write(replace_case['orig_mark'], {'verified': 'unknown'})
+                                    marks_obj.write(replace_case['alt_mark'], {'verified': 'bad'})
+                                    did_something = True
                 
         elif bad_mark['verified'] == 'bad-parents':
             commits = get_commits(bad_mark['commit_ids'])
@@ -269,15 +350,21 @@ def main_loop():
             
             diffs = {}
             for mark_id in first_commit['parent_marks']:
-                if mark_id not in common_marks:
+                if not mark_id:
+                    diffs.setdefault(first_commit['id'],[]).append(False)
+                elif mark_id not in common_marks:
                     diffs.setdefault(first_commit['id'],[]).append(mark_id[1])
             
             for cmt in commits.values():
                 for mark_id in cmt['parent_marks']:
-                    if mark_id not in common_marks:
+                    if not mark_id:
+                        diffs.setdefault(cmt['id'],[]).append(False)
+                    elif mark_id not in common_marks:
                         diffs.setdefault(cmt['id'],[]).append(mark_id[1])
             log.info("Mismatch of parents for %s : %r", bad_mark['mark'], diffs)
-            
+        
+        elif bad_mark['verified'] == 'bad':
+            pass
         else:
             log.warning("Cannot handle %s mark: %s!", bad_mark['verified'], bad_mark['mark'])
 
@@ -291,12 +378,13 @@ if __name__ == "__main__":
         bzr_marks_import(options.opts.bzr_marks)
         log.info("loaded %d bzr marks", len(bzr_marks2))
 
-    npass = 1
+    npass = 0
     while npass < options.opts.num_loops:
+        npass += 1
         log.info("Recover loop %d", npass)
         if not main_loop():
             break
-        npass += 1
+        
 
     log.info('Exiting after %d loops', npass)
 #eof
